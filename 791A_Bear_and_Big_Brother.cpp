fbdc715b4bc814f5b25e7db8b067f5fd1ce8ea48
Amorto Goon <amorto.goon.121@gmail.com>
2026-01-09 19:27:48 +0600

    add 791A_Bear_and_Big_Brother solution

---
+++ 791A_Bear_and_Big_Brother.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int l, b, y = 0;
+
+    cin >> l >> b;
+
+    while (l <= b)
+    {
+        l *= 3;
+        b *= 2;
+        y++;
+    }
+
+    cout << y << endl;
+
+    return 0;
+}
