dc2fd3e6132a51d1df28f7d4719d3f8251596692
Amorto Goon <amorto.goon.121@gmail.com>
2026-06-01 17:02:02 +0600

    add 381A solution

---
+++ 381A_Sereja_and_Dima.cpp
@@ -0,0 +1,60 @@
+/*
+376875958	Jun/01/2026 17:01UTC+6	amorto	381A - Sereja and Dima	C++23 (GCC 14-64, msys2)	Accepted	46 ms	0 KB
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve()
+{
+    int n;
+
+    cin >> n;
+
+    vector<int> cards(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> cards[i];
+    }
+
+    pair<int, int> score = {0, 0};
+    bool is_first_turn = true;
+
+    for (int i = 0; i < n; i++)
+    {
+        int point;
+
+        if (cards.front() > cards.back())
+        {
+            point = cards.front();
+            cards.erase(cards.begin());
+        }
+        else
+        {
+            point = cards.back();
+            cards.pop_back();
+        }
+
+        if (is_first_turn)
+            score.first += point;
+        else
+            score.second += point;
+
+        is_first_turn = !is_first_turn;
+    }
+
+    cout << score.first << " " << score.second << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
